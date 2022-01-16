4f9d09a6c8ae6c72268483514cccbe7be82ff4c8
asbillt <davidasbill@asbillt.com>
2022-01-16 17:30:51 -0600

    Wrap the redux Provider component around the App component.

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@
 import React from "react";
 // Import the ReactDom library.
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./store";
 // Import the App component.
 import App from "./App.js";
 
 // Render the App component into the root div in index.html.
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
