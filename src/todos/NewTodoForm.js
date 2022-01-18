6c06cf655d3216547b1fffbab4553e06e1b50cd5
asbillt <davidasbill@asbillt.com>
2022-01-18 14:16:21 -0600

    Adds the connect function from redux to connect NewTodoForm to Redux Store.

--- src/todos/NewTodoForm.js
+++ src/todos/NewTodoForm.js
@@ -1,11 +1,18 @@
 // Import the core React library.
 // Import the useState hook.
 import React, { useState } from "react";
+// Import connect from the react-redux package.
+// The connect function connects a React component to a Redux store.
+import { connect } from "react-redux";
+// Import the createTodo Redux action.
+import { createTodo } from "./actions";
 // Import styling.
 import "./NewTodoForm.css";
 
 // Create NewTodoForm component.
-const NewTodoForm = () => {
+// Pass in the todos property from mapStateToProps.
+// Pass in the onCreatePressed property from mapDispatchToProps.
+const NewTodoForm = ({ todos, onCreatePressed }) => {
   // Create state variable, inputValue.
   // Create function, setInputValue, to update inputValue.
   // Set initial state set to empty string.
@@ -23,9 +30,47 @@ const NewTodoForm = () => {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
-      <button className="new-todo-button">Create Todo</button>
+      <button
+        onClick={() => {
+          // When a user clicks the Create Todo button, check if the
+          // inputValue that the user is typing (the new todo) is equal to
+          // a todo item (text) already in the todo list.
+          // If they are equal do nothing.
+          // If they are not equal, then pass the inputValue into the onCreatePressed function
+          // and then call the setInputValue function to set the state of inputValue back to
+          // an empty string.
+          const isDuplicateText = todos.some(
+            (todo) => todo.text === inputValue
+          );
+          if (!isDuplicateText) {
+            onCreatePressed(inputValue);
+            setInputValue("");
+          }
+        }}
+        className="new-todo-button"
+      >
+        Create Todo
+      </button>
     </div>
   );
 };
 
-export default NewTodoForm;
+// The state argument passed is an object representing the entire redux state.
+// The mapStateToProps will take the state object and return another object containing
+// the pieces of that state that our component needs access to.
+const mapStateToProps = (state) => ({
+  // Return an object with todos property and value of state.todos.
+  todos: state.todos,
+});
+
+// The dispatch argument is a function that allows the components to trigger
+// actions that the Redux store will respond to.
+// The properties of the object returned will be passed to the component as props.
+const mapDispatchToProps = (dispatch) => ({
+  // Pass the NewTodoForm component the onCreatePressed property.
+  onCreatePressed: (text) => dispatch(createTodo(text)),
+});
+
+// Connect NewTodoForm to Redux store.
+// Component to connect is added in second parentheses.
+export default connect(mapStateToProps, mapDispatchToProps)(NewTodoForm);
