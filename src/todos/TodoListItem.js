56622092ca1e9bc736587bb31751e47773082ce6
asbillt <davidasbill@asbillt.com>
2022-01-19 15:38:39 -0600

    Adds ternary condition and onClick event handler for Mark As Completed button.

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -4,13 +4,26 @@ import React from "react";
 import "./TodoListItem.css";
 
 // Create TodoListItem component.
-// Destructure the todos property.
-const TodoListItem = ({ todo, onRemovePressed }) => {
+// Destructure the todos, onRemovePressed and onCompletedPressed properties.
+const TodoListItem = ({ todo, onRemovePressed, onCompletedPressed }) => {
   return (
     <div className="todo-item-container">
       <h3>{todo.text}</h3>
       <div className="buttons-container">
-        <button className="completed-button">Mark As Completed</button>
+        {
+          // If todo is completed return null (no button) otherwise return the
+          // Mark As Completed button.
+          todo.isCompleted ? null : (
+            <button
+              // On click, call the onCompletedPressed function with the value of
+              // todo.text as the argument.
+              onClick={() => onCompletedPressed(todo.text)}
+              className="completed-button"
+            >
+              Mark As Completed
+            </button>
+          )
+        }
         <button
           // On click, call the onRemovePressed function with the value of
           // todo.text as the argument.
