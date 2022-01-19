135c71338497e338c0c8fbc66ec52e34ce9d1493
asbillt <davidasbill@asbillt.com>
2022-01-19 14:31:38 -0600

    Adds functionality for app state to persist through page refresh.

--- src/index.js
+++ src/index.js
@@ -5,15 +5,28 @@
 import React from "react";
 // Import the ReactDom library.
 import ReactDOM from "react-dom";
+// Import persistStore and PersistGate from the redux-persist library.
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/lib/integration/react";
+// Import Provider from the react-redux library.
 import { Provider } from "react-redux";
 import { configureStore } from "./store";
 // Import the App component.
 import App from "./App.js";
 
+const store = configureStore();
+const persistor = persistStore(store);
+
+// Wrap the App component with Provider to make the Redux store available
+// to any nested components.
+// Wrap the App component with PersistGate to delay rendering of the App's
+// UI until the persisted state has been retrieved and saved to redux.
 // Render the App component into the root div in index.html.
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/store.js
+++ src/store.js
@@ -1,14 +1,35 @@
 // Import the createStore and combineReducers functions from the redux library.
 import { createStore, combineReducers } from "redux";
+// Import persistReducer, storage and autoMergeLevel2 from the redux-persist library.
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 // Import the todos reducers.
 import { todos } from "./todos/reducers";
 
-// Create the reducers object.
+// Create a reducers object that contains reducers imported from
+// the reducers.js file.
+// The todos is an array of the updated state of the UI's todo list.
 const reducers = {
   todos,
 };
 
-// Put the reducers into a form that can be passed to the createStore function.
+// Create a State Reconciler to define how incoming state is merged in
+// with initial state. This with auto merge (shallow merge) 2 levels deep.
+const persistConfig = {
+  key: "root",
+  storage,
+  stateReconciler: autoMergeLevel2,
+};
+
+// Use the combineReducers function to turn the reducers object into
+// a single reducing function in order for it to be passed to the store.
 const rootReducer = combineReducers(reducers);
 
-export const configureStore = () => createStore(rootReducer);
+// Take the rootReducer object and use persistReducer to make it persist
+// through a page refresh. Store the new, persisting reducer in persistedReducer.
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create a Redux Store that holds the complete state tree of the app.
+// Pass the persisting reducer, persistedReducer, to the Redux Store.
+export const configureStore = () => createStore(persistedReducer);
