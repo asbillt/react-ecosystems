cb1f03e873581de12a8f073f3e5241a2481b505d
asbillt <davidasbill@asbillt.com>
2022-01-18 18:28:45 -0600

    Adds redux connect function to TodoList.js. Adds onClick event to button in TodoListItem.js

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,14 +1,18 @@
 // Import the core React library.
 import React from "react";
+// Import connect from the react-redux package.
+// The connect function connects a React component to a Redux store.
+import { connect } from "react-redux";
 import NewTodoForm from "./NewTodoForm";
 // Import TodoListItem.
 import TodoListItem from "./TodoListItem";
+import { removeTodo } from "./actions";
 // Import styling.
 import "./TodoList.css";
 
 // Create TodoList component.
 // Destructure the todos property.
-const TodoList = ({ todos = [] }) => {
+const TodoList = ({ todos = [], onRemovePressed }) => {
   return (
     <div className="list-wrapper">
       <NewTodoForm />
@@ -17,11 +21,29 @@ const TodoList = ({ todos = [] }) => {
         // Pass each todo item into the TodoListItem component
         // through the components' todo property.
         todos.map((todo) => (
-          <TodoListItem todo={todo} />
+          <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />
         ))
       }
     </div>
   );
 };
 
-export default TodoList;
+// The state argument passed is an object representing the entire redux state.
+// The mapStateToProps will take the state object and return another object containing
+// the pieces of that state that our component needs access to.
+const mapStateToProps = (state) => ({
+  // Return an object with todos property and value of state.todos.
+  todos: state.todos,
+});
+
+// The dispatch argument is a function that allows the components to trigger
+// actions that the Redux store will respond to.
+// The properties of the object returned will be passed to the component as props.
+const mapDispatchToProps = (dispatch) => ({
+  // Pass the TodoList component the onRemovePressed property.
+  onRemovePressed: (text) => dispatch(removeTodo(text)),
+});
+
+// Connect NewTodoForm to Redux store.
+// Component to connect is added in second parentheses.
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -5,13 +5,20 @@ import "./TodoListItem.css";
 
 // Create TodoListItem component.
 // Destructure the todos property.
-const TodoListItem = ({ todo }) => {
+const TodoListItem = ({ todo, onRemovePressed }) => {
   return (
     <div className="todo-item-container">
       <h3>{todo.text}</h3>
       <div className="buttons-container">
         <button className="completed-button">Mark As Completed</button>
-        <button className="remove-button">Remove</button>
+        <button
+          // On click, call the onRemovePressed function with the value of
+          // todo.text as the argument.
+          onClick={() => onRemovePressed(todo.text)}
+          className="remove-button"
+        >
+          Remove
+        </button>
       </div>
     </div>
   );
