fbf4651c945f9031a2fc1d1cbb34931b18b6e76a
asbillt <davidasbill@asbillt.com>
2022-01-21 17:36:21 -0600

    Adds 'loading' actions as imports. Creates isLoading reducer.

--- src/todos/reducers.js
+++ src/todos/reducers.js
@@ -9,7 +9,41 @@
 */
 
 // Import the action types from actions.js.
-import { CREATE_TODO, REMOVE_TODO, MARK_TODO_AS_COMPLETED } from "./actions";
+import {
+  CREATE_TODO,
+  REMOVE_TODO,
+  MARK_TODO_AS_COMPLETED,
+  LOAD_TODOS_IN_PROGRESS,
+  LOAD_TODOS_SUCCESS,
+  LOAD_TODOS_FAILURE,
+} from "./actions";
+
+// Create the isLoading reducer.
+// The isLoading reducer will return true or false based on which actions
+// are occurring in the app.
+export const isLoading = (state = false, action) => {
+  // Desctructure the type property from the action parameter.
+  const { type } = action;
+  // Use a switch statement to check for which type/action.
+  switch (type) {
+    // Use the LOAD_TODOS_IN_PROGRESS case when the type is LOAD_TODOS_IN_PROGRESS.
+    case LOAD_TODOS_IN_PROGRESS:
+      // Return the boolean true as the state of isLoading.
+      return true;
+    // Use the LOAD_TODOS_SUCCESS case when the type is LOAD_TODOS_SUCCESS.
+    case LOAD_TODOS_SUCCESS:
+    // Use the LOAD_TODOS_FAILURE case when the type is LOAD_TODOS_FAILURE.
+    case LOAD_TODOS_FAILURE:
+      // Return the boolean false as the state of isLoading for both cases
+      // LOAD_TODOS_SUCCESS and LOAD_TODOS_FAILURE.
+      return false;
+    default:
+      // Default case.
+      // If the action triggered does not match the cases then return
+      // the unchanged state as it currently is.
+      return state;
+  }
+};
 
 // Create the todos reducer.
 export const todos = (state = [], action) => {
