2d14a2f7cdee5b87cbc5f837f04b89a0251fe04d
asbillt <davidasbill@asbillt.com>
2022-01-21 15:29:53 -0600

    Creates 3 new 'loading' action creators in actions.js.

--- src/todos/actions.js
+++ src/todos/actions.js
@@ -1,4 +1,4 @@
-/* Create 2 action creators: functions that create the action objects. (Abstraction) */
+/* Define action creators: functions that create the action objects. (Abstraction) */
 
 // Define the constant for the actions type, CREATE_TODO.
 export const CREATE_TODO = "CREATE_TODO";
@@ -41,4 +41,36 @@ export const markTodoAsCompleted = (text) =>
     type: MARK_TODO_AS_COMPLETED,
     // The payload property assigned the text of the completed todo.
     payload: { text },
+  });
+
+// Define the constant for the actions type, LOAD_TODOS_IN_PROGRESS.
+export const LOAD_TODOS_IN_PROGRESS = "LOAD_TODOS_IN_PROGRESS";
+// 4th action creator:
+export const loadTodosInProgress = () =>
+  // Returns an abstract object.
+  ({
+    // The type property assigned the LOAD_TODOS_IN_PROGRESS constant as its value.
+    type: LOAD_TODOS_IN_PROGRESS,
+  });
+
+// Define the constant for the actions type, LOAD_TODOS_SUCCESS.
+export const LOAD_TODOS_SUCCESS = "LOAD_TODOS_SUCCESS";
+// 5th action creator:
+export const loadTodosSuccess = (todos) =>
+  // Returns an abstract object.
+  ({
+    // The type property assigned the LOAD_TODOS_SUCCESS constant as its value.
+    type: LOAD_TODOS_SUCCESS,
+    // The payload property assigned the todos this action was dispatched with.
+    payload: { todos },
+  });
+
+// Define the constant for the actions type, LOAD_TODOS_FAILURE.
+export const LOAD_TODOS_FAILURE = "LOAD_TODOS_FAILURE";
+// 6th action creator:
+export const loadTodosFailure = () =>
+  // Returns an abstract object.
+  ({
+    // The type property assigned the LOAD_TODOS_FAILURE constant as its value.
+    type: LOAD_TODOS_FAILURE,
   });
